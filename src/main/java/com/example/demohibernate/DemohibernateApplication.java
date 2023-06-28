4ed0a7a5eeb466e31b71ab3b2e4ed6b3f3b61783
Iheb Eljani <iheb.eljani@gmail.com>
2023-06-28 16:38:24 +0200

    ajout de mon projet

---
+++ src/main/java/com/example/demohibernate/DemohibernateApplication.java
@@ -0,0 +1,26 @@
+package com.example.demohibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class DemohibernateApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(DemohibernateApplication.class, args);
+		
+		PersonRepository personneRepository = context.getBean(PersonRepository.class); 
+		Person p = new Person(); 
+		p.setName("Thomas Test");
+		
+		
+		personneRepository.save(p); 
+		
+		for (Person person : personneRepository.findAll()) {
+			System.out.println(person.getName());
+		}
+		
+	}
+
+}
